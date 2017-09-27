9f31cfa08626e37da7e655b6d9f3492d184de610
Sean Simpson <srsimpso@medctr.ad.wfubmc.edu>
2017-09-27 18:54:36 -0400

    Fix flow_OptimalClust() documentation

--- R/flow_OptimalClust.R
+++ R/flow_OptimalClust.R
@@ -2,10 +2,9 @@
 #'
 #' Function implementing various statistical methods to determine the optimal number of clusters for flow cytometry data,
 #'   using various clustering algorithms. ***WARNING*** extremely computationally intensive-- very large data sets may not
-#'   function and/or may take significant time to complete
+#'   function and/or may take significant time to complete.
 #' @param flowObj A data frame or flowFrame to be analyzed
-#' @param algorithm The clustering algorithm to utilize and attempt to generate an optimal cluster number for
-#'   Options (no quotations): kmeans, pam, clara, hcut
+#' @param algorithm The clustering algorithm to utilize. Options (no quotations): kmeans, pam (Partitioning Around Mediods), clara (Clustering LARge Applications), hcut (Hierarchical clustering, cut tree into 'k' clusters)
 #' @param kMax Integer representing max number of possible clusters to consider
 #' @param nBoot Integer representing number of times to repeat the analysis from a random starting position, must be >1 for gap statistic analysis
 #' @param plot Logical indicating whether or not to generate graphical plots for wss, silhouette, and gap statistic methods
